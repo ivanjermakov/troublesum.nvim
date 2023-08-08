ad6fbf187256ac193e09f7144bf0ecfa27bdad2c
ivanjermakov <ivanjermakov1@gmail.com>
2023-08-08 16:22:47 +0200

    Counts

--- lua/troublesum.lua
+++ lua/troublesum.lua
@@ -8,16 +8,44 @@ local display_summary = function(bufnr, ns, text)
     })
 end
 
+local severity_format = {
+    [1] = "E",
+    [2] = "W",
+    [3] = "I",
+    [4] = "H",
+}
+
+local format = function(counts)
+    local str = ""
+    for severity, count in pairs(counts) do
+        if count ~= 0 then
+            if #str ~= 0 then
+                str = str .. " "
+            end
+            str = str .. table.concat({ severity_format[severity], tostring(count) }, " ")
+        end
+    end
+    return str
+end
+
 M.show = function()
     local ns = vim.api.nvim_create_namespace("troublesum")
 
-    local count = tostring(#vim.diagnostic.get())
+    local counts = {
+        [1] = 0,
+        [2] = 0,
+        [3] = 0,
+        [4] = 0,
+    }
+    for _, d in pairs(vim.diagnostic.get(0)) do
+        counts[d.severity] = counts[d.severity] + 1
+    end
 
-    M.hide()
-    display_summary(0, ns, count)
+    M.clear()
+    display_summary(0, ns, format(counts))
 end
 
-M.hide = function()
+M.clear = function()
     local ns = vim.api.nvim_get_namespaces()["troublesum"];
     if ns == nil then return end
     vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
